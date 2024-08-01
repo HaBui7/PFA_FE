9430867db52700e553f3b9a63bc8995155757554
HaBui7 <92909964+HaBui7@users.noreply.github.com>
2024-08-01 14:27:36 +0700

    Update App.tsx

--- financialapp/src/App.tsx
+++ financialapp/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1 className="text-warning md:text-red-400">Hello</h1>
-      <Button className="bg-button ">Click me</Button>
+      <Button className="bg-red-500">Click me</Button>
     </div>
   );
 }
