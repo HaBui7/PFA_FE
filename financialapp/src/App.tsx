3f1b5bfdd9c1bba190ea70c5e7c40b48c464c2fd
HaBui7 <92909964+HaBui7@users.noreply.github.com>
2024-08-01 13:06:39 +0700

    new

--- financialapp/src/App.tsx
+++ financialapp/src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
 import { Button } from "./components/ui/button";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1 className="text-red-300">Hello</h1>
