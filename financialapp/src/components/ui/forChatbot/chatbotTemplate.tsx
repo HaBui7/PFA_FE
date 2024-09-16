fd0abcdc4d77b5de40db2dbeac8167cb33899e3f
Khang Nguyen <duykhang5687@gmail.com>
2024-09-16 20:26:25 +0700

    Fix margin

--- financialapp/src/components/ui/forChatbot/chatbotTemplate.tsx
+++ financialapp/src/components/ui/forChatbot/chatbotTemplate.tsx
@@ -123,7 +123,7 @@ const ChatbotTemplate: React.FC<ChatbotTemplateProps> = ({
         )}
         <main className="overflow-auto h-full w-full sticky">
           {messages && messages.length === 0 ? (
-            <div className="text-center mt-80">
+            <div className="text-center mt-40">
               <h1 className="text-xl font-semibold">Powered by RMIT Val</h1>
               <p className="text-gray-500">Model: GPT-4o</p>
               <p className="text-sm text-gray-400 mt-4">
